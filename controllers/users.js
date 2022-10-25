54e7c0ae3bda50cb96054ccd10fcc8e2835ed6d8
Ibrahim <ibrahimghali211@gmail.com>
2022-10-25 22:18:59 +0300

    add signup controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,25 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 const signup = async (req, res) => {
   const user = await User.create(req.body);
   res.status(201).json({ id: user._id, username: user.username });
 };
-const login = (req, res) => {};
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      res.status(200).json({ id: user._id, username: user.username });
+    } else {
+      throw new Error('Wrong Password!');
+    }
+  } else {
+    throw new Error('Wrong Username!');
+  }
+};
 
 const getUser = (req, res) => {};
 
